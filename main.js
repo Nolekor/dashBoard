4af6d43e65415505164e5b027df6d34a30c9da70
Nolekor <78369850+Nolekor@users.noreply.github.com>
2023-04-12 17:42:12 +0200

    add weather

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+// get time
+const timer = document.getElementById("time");
+
+function getTime() {
+  const date = new Date();
+  timer.innerHTML = date.toLocaleTimeString("en-GB", { timeStyle: "short" });
+}
+
+setInterval(() => getTime(), 1000);
+
+// set background image and author
 async function getImage() {
   const res = await fetch(
     "https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature"
   );
   const data = await res.json();
-  console.log(data);
   document.body.style.backgroundImage = `url(${data.urls.full})`;
   document.getElementById(
     "author"
@@ -11,6 +21,7 @@ async function getImage() {
 }
 getImage();
 
+// set crypto
 async function getCrypto() {
   fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
     .then((res) => {
@@ -33,3 +44,32 @@ async function getCrypto() {
     .catch((err) => console.error(err));
 }
 getCrypto();
+
+// get weather
+
+const weather = document.getElementById("weather");
+
+function getLocation() {
+  navigator.geolocation.getCurrentPosition((position) => {
+    fetch(
+      `https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`
+    )
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("weather was not available");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        weather.innerHTML = `
+        <img src="https://openweathermap.org/img/wn/${
+          data.weather[0].icon
+        }@2x.png" />
+        <p id="temp">${Math.trunc(data.main.feels_like)}°c</p>
+        <p class="city">${data.name}</p>
+        `;
+      })
+      .catch((err) => console.error(err));
+  });
+}
+getLocation();
